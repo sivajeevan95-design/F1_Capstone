01c0f370009f03f04c03ada2d4caea261d94bf43
Patric Mertes <patricmertes@proton.me>
2025-11-25 14:38:28 +0000

    finished last marts

---
+++ models/prep/prep_session.sql
@@ -0,0 +1,11 @@
+select
+    s.*,
+    d.full_name,
+    d.driver_number,
+    p.pos_diff
+from {{ ref("stg_session") }} s
+join {{ ref("stg_position") }} p
+    using (session_key)
+join {{ ref("stg_drivers") }} d
+    using (session_key, driver_number)
+order by s.session_key, d.driver_number
